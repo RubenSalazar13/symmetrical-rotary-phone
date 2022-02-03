3efa220a6022771adaa309bc81afcfe6078e0a3f
Ruben Salazar <94159087+RubenSalazar13@users.noreply.github.com>
2022-02-02 20:57:13 -0700

    Update index.php

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ filter: grayscale(50%);
 <body>
 <center>
   <?php
-include('sqlconnect.php');
+
 include('menubar.php');
 ?> 
 <h5>Tacos in the Beehive Capital</h5>
@@ -87,24 +87,47 @@ include('menubar.php');
   <div class="content">
     <div class="together">
   <img src="img/ruben.jpg" class="ruben" alt="Avatar" width="70" height="70"> 
-<h6>&nbsp; By:Ruben Salazar ~ 16 October 2020 ~</h6></div><br>
+<h6>&nbsp; By:Ruben Salazar ~ 02 February 2022 ~</h6></div><br>
 <p>
-Searching for the best taco in town can be a grueling task. That is why we have compiled the ultimate guide to some of the best Mexican food in the Valley. This list includes the area's local cuisine from West Valley, Riverton, Orem, Ogden, and back. <br><br>
+¡Bienvenidos! Ideally, what makes a great taco vs. the okay ones is the ingredients that should come together as a composed dish. The components that make a great taco are number one, the tortilla; number two, protein or vegetable; whatever's in it, it needs a good flavor. Searching for the great taco in town can be a grueling task.
 
-Yet, if you believe taco bell is authentic Mexican food &#128530; then sadly, this site is not for you. Originally from the metropolitan area, I grew up not knowing what excellent Mexican cuisine lies in our great city. Having traveled all over from Vegas, California, Virginia, and abroad there is no comparison with the local taste! However, there are some establishments around that just are not worth the hype and money. Likewise, with this knowledge, you can avoid the frustration of making the same mistakes that we have.<br><br>
+That is why we have compiled the ultimate guide to some of the best Mexican food in the Valley. This list includes the area's local cuisine from West Valley, Riverton, Orem, Ogden, and back. <br><br>
 
-However, the current Covid epidemic is transforming the dining out exeperience. Therefore, now more than ever we must support local business and we are here for your Taco Tuesday search. In comparison to other sites around, our list is short but spicy.<br><br>
+Yet, if you believe taco bell is authentic Mexican food &#128530; then sadly, this site is not for you. Originally from the metropolitan area, I did not know what excellent Mexican cuisine lies in our great city. Having traveled all over from Nevada, California, Texas, and abroad there is no comparison with the local taste! However, there are some establishments around that just are not worth the hype and money. Likewise, with this knowledge, you can avoid the frustration of making the same mistakes that we have.<br><br>
 
-For example, we rate our following list in beans. If we love it, it then receives five out of five. Unfortuanatly, if we dislike it then it recieves no beans. We hope you enjoy the choices we provided, but if you have your own experiences from the list or better yet have tried a new spot not on our list let us know what you thought of the restuarant from the service, cleaness, Covid-19 reequirements, and best of all the Tacos!
+However, the current Covid epidemic is transforming the dining out experience. Therefore, now more than ever, we must support local businesses, and we are here for your Taco Tuesday search. In comparison to other sites around, our list is short but spicy.<br><br>
+
+For example, we rate our following list in beans. If we love it, it then receives five out of five. Unfortunately, if we dislike it, then it gets no beans. We hope you enjoy the choices we provided, but if you have your own experiences from the list or, better yet, have tried a new spot, not on our list, let us know what you thought of the restaurant from the service, cleanness, Covid-19 requirements, and best of all the Tacos!
 </p>
   </div>
 <div class="content">
 <h1>The Red Iguana</h1>
 <img src="img/RedIguana.jpg" class="center"></div>
+<span class="heading">Our Rating</span>
+<span style="padding-right:3px; padding-top: 3px; display:inline-block;">
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<img class="manImg" src="img/bean2.png" width="25" height="30"></img></span>
 <h2><a href="https://twitter.com/RedIguanaOG">The Red Iguana</a>, 736 W North Temple, Salt Lake City, UT 84116</h2>
 <div class="content">
 <p>First, this list is not complete without one of the valley's most favaortie Mexican spots since 1985.</p>
 
+<div class="content">
+<h1>Taqueria 27</h1>
+<img src="img/taqueria27.jpeg" class="center"></div>
+<span class="heading">Our Rating</span>
+<span style="padding-right:3px; padding-top: 3px; display:inline-block;">
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<h2><a href="https://www.taqueria27.com//">Taqueria 27</a>, Provo, American Fork, Springville and Lindon</h2><br>
+<p>We know that Taqueria 27 Claims to be the best tacos in town. However, this one is the least of our favorites; with their slow service, unauthentic tacos, and overpriced food items, this place can be easily missed without missing much. The only thing this restaurant has is a gorgeous view in their Holladay location. So if your looking for great-tasting tacos, this just is not the place to waste your time.</p><br><br>
+
+<div class="content">
 <h1>La Hacienda</h1>
 <img src="img/LaHacienda.jpg" class="center"></div>
 <span class="heading">Our Rating</span>
@@ -117,10 +140,9 @@ For example, we rate our following list in beans. If we love it, it then receive
 <h2><a href="https://www.facebook.com/media/set/?vanity=LaHaciendaDraper&set=a.2066211183671321">La Hacienda Draper</a>, 12434 Minuteman Dr, Draper, UT 84020</h2><br>
 <p>Next, we hit up a local Draper favorite with good food and a great atmosphere. If you are looking for that mid-range price than this is the spot for you. Dishes here are on average $7.99 before tax and you will not leave here hunger.</p><br><br>
 
+<div class="content">
 <h1>La Costa</h1>
-<img src="img/LaHacienda.jpg" class="center">
-  </div>
-
+<img src="img/LaCosta.jpg" class="center"></div>
 <span class="heading">Our Rating</span>
 <span style="padding-right:3px; padding-top: 3px; display:inline-block;">
 <img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
@@ -128,11 +150,24 @@ For example, we rate our following list in beans. If we love it, it then receive
 <img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
 <img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
 <img class="manImg" src="img/bean2.png" width="25" height="30"></img></span>
-<h2><a href="https://www.facebook.com/media/set/?vanity=LaHaciendaDraper&set=a.2066211183671321">La Hacienda Draper</a>, 12434 Minuteman Dr, Draper, UT 84020</h2><br>
+<h2><a href="https://www.lacostasandy.com/">La Costa</a>, 889 E 9400 S, Sandy, UT 84094</h2><br>
 <p>Next, we hit up a local Draper favorite with good food and a great atmosphere. If you are looking for that mid-range price than this is the spot for you. Dishes here are on average $7.99 before tax and you will not leave here hunger.</p><br><br>
+
+<div class="content">
+<h1>Don Joaquin Street Tacos</h1>
+<img src="img/donjoaquin.jpg" class="center"></div>
+<span class="heading">Our Rating</span>
+<span style="padding-right:3px; padding-top: 3px; display:inline-block;">
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<img class="manImg" src="img/bean.png" width="25" height="30"></img></span>
+<h2><a href="https://stdonjoaquin.com/">Don Joaquin Street Tacos</a>, Provo, American Fork, Springville and Lindon</h2><br>
+<p>Hands down, Don Joaquin's street tacos quickly take the number one spot for not just mouth-watering food but also speed, price, and excellent customer service. Ideally, all the ingredients in a taco should come together as a composed dish. So the components that make a great taco are number one, which Don Joaquin does well with their choices of Asada, Chorizo, Cabeza, Pollo Asado, Campechano, and Al Pastor. Next, the tortilla, followed by protein or vegetable; whatever's in it, it needs a good flavor.</p><br><br>
 <?php
-include_once('form35.php');
-include_once('footer.php');
+include('form35.php');
+include('footer.php');
 ?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
